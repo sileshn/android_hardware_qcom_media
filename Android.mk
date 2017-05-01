pablomh/platform_hardware_qcom_media

media: Add support for msm8916
Enable media support for msm8916 target.

Change-Id: Ie76ce1161dc07cebe8f2a470859b06c0b477d5c1

Conflicts:
	Android.mk

Android.mk
@@ -9,7 +9,7 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     QCOM_MEDIA_ROOT := $(call my-dir)/msmcobalt
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8952 msm8974 msm8992 msm8994 msm8996 msmcobalt,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8610 msm8226 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8916 msm8952 msm8974 msm8992 msm8994 msm8996 msmcobalt,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
     include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
   endif
@@ -18,11 +18,11 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     include $(QCOM_MEDIA_ROOT)/mm-video-legacy/Android.mk
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8084 msm8952 msm8974 msm8992 msm8994 msm8996 msmcobalt,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8610 msm8226 msm8084 msm8916 msm8952 msm8974 msm8992 msm8994 msm8996 msmcobalt,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8960 msm8084 msm8952 msm8974 msm8992 msm8994 msm8996 msmcobalt,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8610 msm8226 msm8960 msm8084 msm8916 msm8952 msm8974 msm8992 msm8994 msm8996 msmcobalt,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
   endif
 endif

sileshn/android_hardware_qcom_media

Android.mk
@@ -11,7 +11,7 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     QCOM_MEDIA_ROOT := $(call my-dir)/msm8998
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8916 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
     include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
   endif
@@ -20,11 +20,11 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     include $(QCOM_MEDIA_ROOT)/mm-video-legacy/Android.mk
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8974 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8610 msm8226 msm8974 msm8084 msm8916 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8084 msm8916 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
   endif
 endif